b76625d46a9454c37595a339414c40ee24753336
Paulo <paulo.zti@gmail.com>
2020-06-20 22:49:06 -0300

    First commit

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float _speed = 1;
+    public float _upRangeDelay = 1;
+    public float _downRangeDelay = 1;
+    [Header("Difficult Level")]
+    public float _DifficultSpeed = 5f;
+
+    public static float speed;
+    public static float obstacleSpawnDelay;
+
+    private bool changeSpawnDelay = true;
+    private void Start()
+    {
+        obstacleSpawnDelay = _upRangeDelay;
+    }
+    void Update()
+    {
+        if(speed != _speed)
+        {
+            speed = _speed;
+        }
+        if (changeSpawnDelay)
+        {
+            StartCoroutine(ChangeSpawnDelay());
+        }
+    }
+
+    private IEnumerator ChangeSpawnDelay()
+    {
+        changeSpawnDelay = false;
+        yield return new WaitForSeconds(_DifficultSpeed);
+        
+        if((_upRangeDelay > 2))
+        {
+            _upRangeDelay -= 0.25f;
+            if (_upRangeDelay < 2)
+                _upRangeDelay = 2;
+        }
+        if(_downRangeDelay > 1)
+        {
+            _downRangeDelay -= 0.25f;
+            if (_downRangeDelay < 1)
+                _downRangeDelay = 1;
+        }
+        
+        obstacleSpawnDelay = Random.Range(_downRangeDelay, _upRangeDelay);
+        changeSpawnDelay = true;
+    }
+}
---
+++ Assets/Resources/Scripts/GroundTeleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTeleporter : MonoBehaviour
+{
+    [SerializeField] Transform positionToTeleport;
+    private Vector3 position;
+    private void Start()
+    {
+        position = positionToTeleport.position;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.position = position;
+    }
+}
